d4c49c63576d20287d5c275fdd14578f198aa207
Nicelx <relush321@gmail.com>
2025-03-06 19:11:15 +0300

    update

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const app = express();
 const PORT = 5000;
 
+
+app.use(express.static(path.join(__dirname, '../client/build')));
 // Middleware
 app.use(cors());
 app.use(express.json());
@@ -11,7 +14,13 @@ app.use(express.json());
 app.get('/api/data', (req, res) => {
   res.json({ message: 'Hello from Express!' });
 });
-
+app.post('/api/submit', (req, res) => {
+    const { name } = req.body;
+    res.json({ status: 'Success', receivedName: name });
+  });
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
 // Запуск сервера
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
